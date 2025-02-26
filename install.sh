e9e0d9fd46a7e1bc4306d337120dd14a93f1305e
Sidharth Rao <sidharthmrao@gmail.com>
2025-02-25 22:31:19 -0500

    Add godot sim

--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 # Add any installation steps here
+apt install -y \
+    libeigen3-dev \
+    libnlopt-dev \
+    libnlopt-cxx-dev \
+    libopencv-dev \
+    libyaml-cpp-dev \
+    libsuitesparse-dev \
+    libceres-dev \
+    ros-humble-slam-toolbox
